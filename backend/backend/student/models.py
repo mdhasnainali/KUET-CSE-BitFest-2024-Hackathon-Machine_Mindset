14cbd7bba974356a2548b069adf50219ffebbc31
tanimsk <sktanim5800@gmail.com>
2025-01-03 12:14:48 +0600

    roles added

--- backend/backend/student/models.py
+++ backend/backend/student/models.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 class Student(models.Model):
     student = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="teacher"
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="student"
     )
     name = models.CharField(max_length=100)
     roll = models.CharField(max_length=100)
     level = models.CharField(max_length=100)
 
     def __str__(self) -> str:
-        return f"{self.agency_name} {self.agent.email}"
+        return f"{self.student} {self.student.email}"
